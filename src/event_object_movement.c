38a16c2bfa56ca2d3762514817467bcbc1853f94
Kasen <forerunnerin@hotmail.com>
2024-09-11 09:35:36 +0300

    Revert deprecated flag

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -2152,8 +2152,7 @@ void UpdateFollowingPokemon(void)
      || !GetFollowerInfo(&species, &form, &shiny)
      || SpeciesToGraphicsInfo(species, form) == NULL
      || (gMapHeader.mapType == MAP_TYPE_INDOOR && SpeciesToGraphicsInfo(species, form)->oam->size > ST_OAM_SIZE_2)
-     || FlagGet(FLAG_TEMP_HIDE_FOLLOWER)
-     || FlagGet(FLAG_DISABLE_FOLLOWER))
+     || FlagGet(FLAG_TEMP_HIDE_FOLLOWER))
     {
         RemoveFollowingPokemon();
         return;
