57c22e83f985d726575af80638096ddcb852bd25
StickieAnimates <139497748+stickiedoesntanimate@users.noreply.github.com>
2024-11-21 21:37:51 -0500

    Fix Most Build Errors

--- include/config/general.h
+++ include/config/general.h
@@ -70,7 +70,7 @@
 #define EXPANSION_INTRO              FALSE    // If TRUE, a custom RHH intro will play after the vanilla copyright screen.
 #define POKEDEX_PLUS_HGSS            TRUE   // If TRUE, enables the custom HGSS style Pokedex.
 #define SUMMARY_SCREEN_NATURE_COLORS TRUE    // If TRUE, nature-based stat boosts and reductions will be red and blue in the summary screen.
-#define HQ_RANDOM                    TRUE    // If TRUE, replaces the default RNG with an implementation of SFC32 RNG. May break code that relies on RNG.
+#define HQ_RANDOM                    FALSE    // If TRUE, replaces the default RNG with an implementation of SFC32 RNG. May break code that relies on RNG.
 #define COMPETITIVE_PARTY_SYNTAX     TRUE    // If TRUE, parties are defined in "competitive syntax".
 
 // Measurement system constants to be used for UNITS
