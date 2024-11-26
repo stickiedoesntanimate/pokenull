76cf664174adb501d9761f35513df98116661924
StickieAnimates <139497748+stickiedoesntanimate@users.noreply.github.com>
2024-11-26 09:48:59 -0500

    Get Match Calls Only If Caller Wants a Rematch

--- src/match_call.c
+++ src/match_call.c
@@ -1109,7 +1109,8 @@ static bool32 SelectMatchCallTrainer(void)
         return FALSE;
 
     matchCallId = GetTrainerMatchCallId(sMatchCallState.trainerId);
-    if (GetRematchTrainerLocation(matchCallId) == gMapHeader.regionMapSectionId && !TrainerIsEligibleForRematch(matchCallId))
+    if (!((TrainerIsEligibleForRematch(matchCallId) && GetRematchTrainerLocation(matchCallId) == gMapHeader.regionMapSectionId)
++   || ShouldTrainerRequestBattle(matchCallId)))
         return FALSE;
 
     return TRUE;
